02425a9fc58fe276b5123df980810bcf1c3cd6b9
Jegp <jensep@gmail.com>
2013-10-31 23:04:47 +0100

    Finalised prodcons code

--- oo2/prodcons.c
+++ oo2/prodcons.c
@@ -8,9 +8,11 @@
 
 int max_buffer_size = 0;
 int max_products = 0;
+int consumed = 0;
+int produced = 0;
 List * buffer;
-pthread_mutex_t buffer_access;
-sem_t empty, full, products_consumed, products_produced;
+pthread_mutex_t buffer_access, products_consumed, products_produced;
+sem_t empty, full;
 
 /* Random sleep function */
 void Sleep(float wait_time_ms) {
@@ -18,52 +20,62 @@ void Sleep(float wait_time_ms) {
   usleep((int) (wait_time_ms * 1e3f)); // convert from ms to us
 }
 
-int canConsume(void) {
-  int products;
-  sem_getvalue(&products_consumed, &products);
-  return products < max_products;
+int should_consume(void) {
+  int r = 0;
+  pthread_mutex_lock(&products_consumed);
+  if (consumed < max_products) {
+    consumed++;
+    r = consumed;
+  }
+  pthread_mutex_unlock(&products_consumed);
+  return r;
 }
 
-int canProduce(void) {
-  int products;
-  sem_getvalue(&products_produced, &products);
-  return products < max_products;
+int should_produce(void) {
+  int r = 0;
+  pthread_mutex_lock(&products_produced);
+  if (produced < max_products) {
+    produced++;
+    r = produced;
+  }
+  pthread_mutex_unlock(&products_produced);
+  return r;
 }
 
 void * consume(void * param) {
   Node * node;
-  int id = (int) param;
-
-  while(canConsume()) {
+  int * id = (int *) param;
+  
+  // Loop
+  while (should_consume()) {
     Sleep(200);
     sem_wait(&full);
     node = list_remove(buffer);
-    printf("Consumer %3d consumed %6s. Items in buffer: %3d (out of %3d).\n", id, node->elm, buffer->len, max_buffer_size);
     sem_post(&empty);
-    sem_post(&products_consumed);
+    printf("Consumer %3d consumed %6s. Items in buffer: %3d (out of %3d).\n", *id, (char *) node->elm, buffer->len, max_buffer_size);
   }
   pthread_exit(0);
 }
 
 void * produce(void * param) {
   Node * node;
-  int id = (int) param;
+  int * id = (int *) param;
+  int name;
 
-  while(canProduce()) {
+  while((name = should_produce())) {
     Sleep(200);
     sem_wait(&empty);
-    char str[6];
-    sprintf(str, "Elm %d", buffer->len);
+    char str[7];
+    sprintf(str, "Elm %d", name);
     node = node_new_str(str);
     list_add(buffer, node);
     sem_post(&full);
-    printf("Producer %3d produced %6s. Items in buffer: %3d (out of %3d).\n", id, str, buffer->len, max_buffer_size);
-    sem_post(&products_produced);
+    printf("Producer %3d produced %6s. Items in buffer: %3d (out of %3d).\n", *id, str, buffer->len, max_buffer_size);
   }
   pthread_exit(0);
 }
 
-pthread_t start_thread(int cons, int id) {
+pthread_t start_thread(int cons, int * id) {
   pthread_t t;
   pthread_attr_t attr;
   pthread_attr_init(&attr);
@@ -78,7 +90,7 @@ pthread_t start_thread(int cons, int id) {
 int main(int argc, char ** argv) {
   if (argc != 5) {
     printf("Usage: [Number of producers] [Number of consumers] [Size of shared buffer] [Number of products to produce]");
-    return 1;
+    exit(1);
   }
 
   // seed the random number generator
@@ -94,16 +106,25 @@ int main(int argc, char ** argv) {
   max_products   = atoi(argv[4]);
   int i;
   max_buffer_size = atoi(argv[3]);
+
+  if (!(producers && consumers)) {
+    printf("Error: Must have more than 0 producers and consumers.\n");
+    exit(2);
+  }
+
   buffer = list_new();
   printf("Initialising %d producers and %d consumers with a buffer size of %d\n", producers, consumers, max_buffer_size);
 
   pthread_mutex_init(&buffer_access, NULL);
+  pthread_mutex_init(&products_produced, NULL);
+  pthread_mutex_init(&products_consumed, NULL);
+  
   sem_init(&empty, 0, max_buffer_size);
   sem_init(&full, 0, 0);
 
   // Start producers and consumers
-  for (i = 0; i < producers; i++) ps[i] = start_thread(0, i);
-  for (i = 0; i < consumers; i++) cs[i] = start_thread(1, i);
+  for (i = 0; i < producers; i++) ps[i] = start_thread(0, &i);
+  for (i = 0; i < consumers; i++) cs[i] = start_thread(1, &i);
     
   // Join producers and consumers
   for (i = 0; i < producers; i++) pthread_join(ps[i], NULL);
